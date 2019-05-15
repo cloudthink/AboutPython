c8b9ac47f944d13f3c367b74f160d9a001416490
Jinming Yang <944596544@qq.com>
2019-05-15 09:53:57 +0800

    Update DNN.py
    修改模型为短时易混4语种分类模型，部分参数调整；原先各方法中的sess参数取消直接用self.sess；BUG修改

--- AboutDL/语种识别/DNN.py
+++ AboutDL/语种识别/DNN.py
@@ -12,7 +12,7 @@ from rbm import RBM
 from YZSB import FixNN
 
 class DNN(object):
-    def __init__(self, n_in, n_out=3, hidden_layers_sizes=[2048, 2048, 50, 2048, 2048]):
+    def __init__(self, n_in, n_out=4, hidden_layers_sizes=[2048, 2048, 50, 2048, 2048]):
         assert len(hidden_layers_sizes) > 0
         self.rbm_layers = []
         self.sess = tf.Session()
@@ -34,11 +34,11 @@ class DNN(object):
             #sum_W = tf.matmul(layer_input, W) + b
             sum_W = tf.add(tf.matmul(layer_input, W), b, name="HiddenLayer{}".format(i))
             t_layer_input = tf.nn.sigmoid(sum_W)
-            #if i > 0 and hidden_layers_sizes[i-1] > hidden_layers_sizes[i]:
-            #    self.DBF = t_layer_input
+            if i > 0 and hidden_layers_sizes[i-1] > hidden_layers_sizes[i]:
+                self.DBF = t_layer_input
             # 创建RBM层
             self.rbm_layers.append(RBM(inpt=layer_input, n_visiable=input_size, n_hidden=hidden_layers_sizes[i],W=W, hbias=b))
-            if i > 0 and hidden_layers_sizes[i-1] > hidden_layers_sizes[i]:
+            if i > 0 and hidden_layers_sizes[i] > hidden_layers_sizes[i-1]:
                 self.DBF = self.rbm_layers[i].input
             layer_input = t_layer_input
 
@@ -51,9 +51,7 @@ class DNN(object):
         self.accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
     #预训练
-    def pretrain(self,X_train,sess=None, batch_size=100, pretraining_epochs=10, lr=0.005, k=1, display_step=1):
-        if sess is None:
-            sess = self.sess
+    def pretrain(self,X_train,batch_size=150, pretraining_epochs=10, lr=0.005, k=1, display_step=1):
         print('开始预训练...\n')
         start_time = timeit.default_timer()
         batch_num = int(X_train.train.num_examples / batch_size)
@@ -65,57 +63,54 @@ class DNN(object):
                 avg_cost = 0.0
                 for _ in range(batch_num):
                     x_batch, _ = X_train.train.next_batch(batch_size)
-                    sess.run(train_ops, feed_dict={self.x: x_batch,})
-                    avg_cost += sess.run(cost, feed_dict={self.x: x_batch,}) / batch_num
+                    self.sess.run(train_ops, feed_dict={self.x: x_batch,})
+                    avg_cost += self.sess.run(cost, feed_dict={self.x: x_batch,}) / batch_num
                 if epoch % display_step == 0:
                     print("\t预训练层 {0} 步数 {1} :{2}".format(i+1, epoch+1,avg_cost))
         end_time = timeit.default_timer()
         print("\n预训练进程用时 {0} 分钟".format((end_time - start_time) / 60))
     
-    #训练拟合神经网络
-    def finetuning(self, trainSet,sess=None,training_epochs=500, batch_size=100, lr=0.1,display_step=1,med_epochs=400):
-        if sess is None:
-            sess = self.sess
-        print("\nDNN训练...\n")
+    #精细调整
+    def finetuning(self, trainSet,training_epochs=600, batch_size=150, lr=0.12,display_step=1,med_epochs=None):
+        if med_epochs is None:
+            med_epochs=training_epochs/2
+        print("\nDNN精细调整...\n")
         start_time = timeit.default_timer()
         train_op = tf.train.GradientDescentOptimizer(learning_rate=lr).minimize(self.loss)
         epoch = 1
         maxAcc = 0
+        batch_num = int(trainSet.train.num_examples / batch_size)
         while epoch <= training_epochs and epoch<=med_epochs:
             avg_loss = 0.0
-            batch_num = int(trainSet.train.num_examples / batch_size)
             for _ in range(batch_num):
                 x_batch, y_batch = trainSet.train.next_batch(batch_size)
-                # 训练
-                sess.run(train_op, feed_dict={self.x: x_batch, self.y: y_batch})
-                avg_loss += sess.run(self.loss, feed_dict= {self.x: x_batch, self.y: y_batch}) / batch_num
+                self.sess.run(train_op, feed_dict={self.x: x_batch, self.y: y_batch})
+                avg_loss += self.sess.run(self.loss, feed_dict= {self.x: x_batch, self.y: y_batch}) / batch_num
             if epoch % display_step == 0:
-                val_acc = sess.run(self.accuracy, feed_dict={self.x: trainSet.validation.wavs,self.y: trainSet.validation.labels})
+                val_acc = self.sess.run(self.accuracy, feed_dict={self.x: trainSet.validation.wavs,self.y: trainSet.validation.labels})
                 maxAcc = val_acc if val_acc > maxAcc else maxAcc
                 print("\t训练步数 {0} 校验精度确认: {1} 损失值:{2}".format(epoch, val_acc,avg_loss))
-            if epoch == training_epochs:
-                conFlag = input('达到最大训练步数，是否增加步数继续训练？Yes/No：')
-                if conFlag == 'Yes':
-                    training_epochs += int(input('请输入延长的步数（整数）：'))
+            #if epoch == training_epochs:
+            #    conFlag = input('达到最大训练步数，是否增加步数继续训练？Yes/No：')
+            #    if conFlag == 'Yes':
+            #        training_epochs += int(input('请输入延长的步数（整数）：'))
             epoch += 1
         while epoch>med_epochs and epoch <= training_epochs:
             if epoch % 10 == 1:#在特定的med_epochs步数之后进入手动学习率衰减训练，每10步一减
                 train_op = tf.train.GradientDescentOptimizer(learning_rate=lr-0.0001*(epoch-med_epochs)).minimize(self.loss)
             avg_loss = 0.0
-            batch_num = int(trainSet.train.num_examples / batch_size)
             for _ in range(batch_num):
                 x_batch, y_batch = trainSet.train.next_batch(batch_size)
-                # 训练
-                sess.run(train_op, feed_dict={self.x: x_batch, self.y: y_batch})
-                avg_loss += sess.run(self.loss, feed_dict= {self.x: x_batch, self.y: y_batch}) / batch_num
+                self.sess.run(train_op, feed_dict={self.x: x_batch, self.y: y_batch})
+                avg_loss += self.sess.run(self.loss, feed_dict= {self.x: x_batch, self.y: y_batch}) / batch_num
             if epoch % display_step == 0:
-                val_acc = sess.run(self.accuracy, feed_dict={self.x: trainSet.validation.wavs,self.y: trainSet.validation.labels})
+                val_acc = self.sess.run(self.accuracy, feed_dict={self.x: trainSet.validation.wavs,self.y: trainSet.validation.labels})
                 maxAcc = val_acc if val_acc > maxAcc else maxAcc
                 print("\t训练步数 {0} 校验精度确认: {1} 损失值:{2}".format(epoch, val_acc,avg_loss))
-            if epoch == training_epochs:
-                conFlag = input('达到最大训练步数，是否增加步数继续训练？Yes/No：')
-                if conFlag == 'Yes':
-                    training_epochs += int(input('请输入延长的步数（整数）：'))
+            #if epoch == training_epochs:
+            #    conFlag = input('达到最大训练步数，是否增加步数继续训练？Yes/No：')
+            #    if conFlag == 'Yes':
+            #        training_epochs += int(input('请输入延长的步数（整数）：'))
             epoch += 1
         print('最佳校验精度：{}'.format(maxAcc))
         end_time = timeit.default_timer()
@@ -155,38 +150,36 @@ class DNN(object):
         print("加载模型来自 " + folder)
         return self
 
-    def save(self,sess=None, modelName='yzsb', path='D:\\YZSB'):
-        if sess is None:
-            sess = self.sess
+    def save(self,modelName='yzsb', path='D:\\YZSB'):
         folder = os.path.join(path, "{}".format(modelName),"Model.ckpt")
         if not os.path.isdir(folder):
             os.makedirs(folder)
         print("保存模型中...")
         saver = tf.train.Saver()
-        saver.save(sess, folder)
+        saver.save(self.sess, folder)
         print("保存完成")
 
     #可视化日志路径：命令行tensorboard --logdir=D:\tmp\tbLogs --host=127.0.0.1
     def prepare_tensorboard_verbose(self):
         try:
-            tb_log_folder = os.path.join(os.path.sep, "D;\\tmp", "tbLogs",
+            tb_log_folder = os.path.join(os.path.sep, "D:\\YZSB\\tmp", "tbLogs",
                 str(datetime.datetime.now())[:19].replace(":", "-"))
             train_dir = os.path.join(tb_log_folder, "train")
             tf.summary.merge_all()
             tf.summary.FileWriter(train_dir, self.sess.graph)
-        except:
-            print('模型可视化保存异常')
+        except BaseException as e:
+            print('模型可视化保存异常：{}'.format(e))
         
 
 #通过命令行模式启动时执行
 if __name__ == "__main__":
     data = input_data.read_data_sets("C:\\DataSet\\")
-    dnn = DNN(n_in=input_data.mfcc_length*input_data.frame_length, n_out=4, hidden_layers_sizes=[2048, 2048, 50, 2048, 2048])
-    modelName = '4yzsb800'#保存和加载模型的名字
+    dnn = DNN(n_in=input_data.mfcc_length*input_data.frame_length, n_out=len(input_data.lab_dict), hidden_layers_sizes=[2048, 2048, 50, 2048, 2048])
+    modelName = '4yzsb600'#保存和加载模型的名字
     if os.path.exists(os.path.join('D:\\YZSB',modelName,"Model.ckpt.meta")):
         dnn.load(modelName=modelName)
         dnn.TestAcc(trainSet=data)
-        dnn.predect(data.test.wavs[:10],data.test.labels[:10])
+        #dnn.predect(data.test.wavs[:10],data.test.labels[:10])
         dnn.prepare_tensorboard_verbose()
     else:
         init = tf.global_variables_initializer()
@@ -195,11 +188,12 @@ if __name__ == "__main__":
         dnn.pretrain(X_train=data)
         dnn.finetuning(trainSet=data)
         dnn.save(modelName=modelName)
+        dnn.prepare_tensorboard_verbose()
 
-    yzsb = FixNN()
-    x1 = dnn.GetDeepFeature(data.train.wavs)
-    x2 = dnn.GetDeepFeature(data.validation.wavs)
+    yzsb = FixNN(shape=50)
+    x1,x2 = dnn.GetDeepFeature(data.train.wavs),dnn.GetDeepFeature(data.validation.wavs)
     x = np.concatenate((x1,x2),axis=0)
     y = np.concatenate((data.train.labels, data.validation.labels),axis=0)
     yzsb.fit(x,y,dnn.GetDeepFeature(data.test.wavs), data.test.labels)
-    a = input('训练完成')
+    #yzsb.predict(dnn.GetDeepFeature(data.test.wavs[:10]),data.test.labels[:10])
+    input('训练完成,输入AnyKeyboard结束：')
