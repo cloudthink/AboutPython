563f9d00ecaf765194b84c6a57e488bc6946eae9
yangjinming1062 <944596544@qq.com>
2019-05-24 09:35:37 +0800

    增加：人工决定训练进程；模型保存bp文件。优化：优化语言学模型训练过程显示，进度条显示模型进程

--- AboutDL/语音识别/train.py
+++ AboutDL/语音识别/train.py
@@ -1,12 +1,14 @@
 import os
 import tensorflow as tf
 import utils
+from tqdm import tqdm
+import keras
 from keras.callbacks import ModelCheckpoint,EarlyStopping,TensorBoard
+from tensorflow.python.framework import graph_util, graph_io
 
-from keras.backend.tensorflow_backend import set_session
 config = tf.ConfigProto()
 config.gpu_options.allow_growth=True   #不全部占满显存, 按需分配
-set_session(tf.Session(config=config))
+keras.backend.tensorflow_backend.set_session(tf.Session(config=config))
 
 
 # 0.准备训练所需数据------------------------------
@@ -17,7 +19,6 @@ data_args.aishell = False
 data_args.prime = True
 data_args.stcmd = False
 data_args.batch_size = 2
-data_args.data_length = None
 data_args.shuffle = True
 train_data = utils.get_data(data_args)
 
@@ -30,10 +31,11 @@ data_args.aishell = False
 data_args.prime = True
 data_args.stcmd = False
 data_args.batch_size = 2
-data_args.data_length = None
 data_args.shuffle = True
 dev_data = utils.get_data(data_args)
 
+
+batch_num = len(train_data.wav_lst) // train_data.batch_size
 
 # 1.声学模型训练-----------------------------------
 from model_speech.cnn_ctc import Am, am_hparams
@@ -44,29 +46,48 @@ am_args.lr = 0.0008
 am_args.is_training = True
 am = Am(am_args)
 
-
-if os.path.exists(os.path.join(utils.cur_path,'logs_am/model.h5')):
+flag = os.path.exists(os.path.join(utils.cur_path,'logs_am/model.h5'))
+if flag:
     print('加载声学模型...')
     am.ctc_model.load_weights(os.path.join(utils.cur_path,'logs_am/model.h5'))
 
-epochs = 100
-batch_num = len(train_data.wav_lst) // train_data.batch_size
+if flag and input('已有保存的声学模型，是否继续训练 yes/no') == 'no':
+    pass
+else:
+    checkpoint = ModelCheckpoint(os.path.join(utils.cur_path,'checkpoint', "model_{epoch:02d}-{val_loss:.2f}.h5"), monitor='val_loss',save_best_only=True)
+    eStop = EarlyStopping(patience=2)#损失函数不再减小后patience轮停止训练
+    tensbrd = TensorBoard(log_dir='logs_am/tbLog')
+    batch = train_data.get_am_batch()#获取的是生成器
+    dev_batch = dev_data.get_am_batch()
+    validate_step = 100#取N个验证的平均结果
 
-# checkpoint
-checkpoint = ModelCheckpoint(os.path.join(utils.cur_path,'checkpoint', "model_{epoch:02d}-{val_loss:.2f}.h5"), monitor='val_loss',save_best_only=True)
-eStop = EarlyStopping(patience=2)#损失函数不再减小后patience轮停止训练
-#tensbrd = TensorBoard(log_dir='./tmp/tbLog')
-cbList =[checkpoint,eStop]
-batch = train_data.get_am_batch()#获取的是生成器
-dev_batch = dev_data.get_am_batch()
-validate_step = 100#取N个验证的平均结果
-history = am.ctc_model.fit_generator(batch, steps_per_epoch=batch_num, epochs=epochs, callbacks=cbList,
-    workers=1, use_multiprocessing=False,verbose=1,validation_data=dev_batch, validation_steps=validate_step)
-am.ctc_model.save_weights(os.path.join(utils.cur_path,'logs_am', 'model.h5'))
+    history = am.ctc_model.fit_generator(batch, steps_per_epoch=batch_num, epochs=100, callbacks=[checkpoint,eStop,tensbrd],
+        workers=1, use_multiprocessing=False,verbose=1,validation_data=dev_batch, validation_steps=validate_step)
+    am.ctc_model.save_weights(os.path.join(utils.cur_path,'logs_am/model.h5'))
+    #写入序列化的 PB 文件
+    with keras.backend.get_session() as sess:
+        frozen_graph = freeze_session(sess, output_names=['outputs'])
+        graph_io.write_graph(frozen_graph, os.path.join(utils.cur_path,'logs_am') 'amModel.pb', as_text=False)
+
+def freeze_session(session, keep_var_names=None, output_names=None, clear_devices=False):
+    graph = session.graph
+    with graph.as_default():
+    freeze_var_names = list(set(v.op.name for v in tf.global_variables()).difference(keep_var_names or []))
+    output_names = output_names or []
+    output_names += [v.op.name for v in tf.global_variables()]
+    input_graph_def = graph.as_graph_def()
+
+    if clear_devices:
+        for node in input_graph_def.node:
+            node.device = ""
+    frozen_graph = graph_util.convert_variables_to_constants(session, input_graph_def,output_names, freeze_var_names)
+    return frozen_graph
+
 
 
 # 2.语言模型训练-------------------------------------------
 from model_language.transformer import Lm, lm_hparams
+import numpy as np
 lm_args = lm_hparams()
 lm_args.num_heads = 8
 lm_args.num_blocks = 6
@@ -79,7 +100,9 @@ lm_args.lr = 0.0003
 lm_args.is_training = True
 lm = Lm(lm_args)
 
-epochs = 100
+epochs = 80
+batch_num_list = [i for i in range(batch_num)]#为进度条显示每一个epoch中的进度用
+loss_list=[]#记录每一步平均损失的列表，实现提前终止训练功能：每次取出后N个数据的平均值和当前的平均损失值作比较
 with lm.graph.as_default():
     saver =tf.train.Saver()
 with tf.Session(graph=lm.graph) as sess:
@@ -95,7 +118,7 @@ with tf.Session(graph=lm.graph) as sess:
     for k in range(epochs):
         total_loss = 0
         batch = train_data.get_lm_batch()
-        for i in range(batch_num):
+        for i in tqdm(batch_num_list,ncols=90):
             input_batch, label_batch = next(batch)
             feed = {lm.x: input_batch, lm.y: label_batch}
             cost,_ = sess.run([lm.mean_loss,lm.train_op], feed_dict=feed)
@@ -103,6 +126,17 @@ with tf.Session(graph=lm.graph) as sess:
             if (k * batch_num + i) % 10 == 0:
                 rs=sess.run(merged, feed_dict=feed)
                 writer.add_summary(rs, k * batch_num + i)
-        print('步数', k+1, ': 平均损失值 = ', total_loss/batch_num)
+        avg_loss = total_loss/batch_num
+        print('步数', k+1, ': 平均损失值 = ', avg_loss)
+
+        loss_list.append(avg_loss)
+        if avg_loss>np.mean(loss_list[-10:])-0.01:
+            if input('模型性能已无法提升，是否提前结束训练？ yes/no')=='yes':
+                break
+        
     saver.save(sess, os.path.join(utils.cur_path,'logs_lm/model_%d' % (epochs + add_num)))
     writer.close()
+    # 写入序列化的 PB 文件
+    constant_graph = graph_util.convert_variables_to_constants(sess, sess.graph_def,output_node_names='preds')
+    with tf.gfile.FastGFile(os.path.join(utils.cur_path,'logs_lm','lmModel.pb'), mode='wb') as f:
+        f.write(constant_graph.SerializeToString())
