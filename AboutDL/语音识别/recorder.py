8032dacdecf19f0447d8d36b9e30d5e12c2f0ada
yangjinming1062 <944596544@qq.com>
2019-06-03 15:46:25 +0800

    修改实时图显示；调整启停参数；针对读取文件情况的异常处理

--- AboutDL/语音识别/recorder.py
+++ AboutDL/语音识别/recorder.py
@@ -33,7 +33,7 @@ class SubplotAnimation(animation.TimedAnimation):
         你好啊
         当一个指定时间内没有明显声音时则清空
         '''
-        self.data=[]
+        self.data=np.array([])
         self.resHan=[]#语音识别结果，类型待定
 
         fig = plt.figure(num='Real-time wave')
@@ -41,14 +41,12 @@ class SubplotAnimation(animation.TimedAnimation):
         ax2 = fig.add_subplot(2, 1, 2)#两行一列，第二子图
 
         self.t = np.linspace(0, self.chunk - 1, self.chunk)
-        ax1.set_xlabel('t')
-        ax1.set_ylabel('x')
+        #ax1.set_xlabel('t')
+        #ax1.set_ylabel('x')
         self.line1, = ax1.plot([], [], lw=2)
         ax1.set_xlim(0, self.chunk)
         ax1.set_ylim(-10000, 10000)
 
-        ax2.set_xlabel('hz')
-        ax2.set_ylabel('y')
         self.line2, = ax2.plot([], [], lw=2)
         ax2.set_xlim(0, self.chunk)
         ax2.set_ylim(-10, 50)
@@ -58,35 +56,40 @@ class SubplotAnimation(animation.TimedAnimation):
 
 
     def _draw_frame(self, framedata):
-        #i = framedata
         x = np.linspace(0, self.chunk - 1, self.chunk)
         y = np.fromstring(self.read(self.chunk), dtype=np.int16)
-        #默认最短3秒为每段话的间隔 3*1000/25=120：只要说话内容间隔3秒以上即清除之前的
-        if len(y)==0:
+        special_flag = False#特殊判断标记，当最后一段音频不足时赋值为真，主要就是针对读取固定长度音频的情况
+        if len(y) == 0:
             return
-        self.data.append(y)
-        if np.array(self.data[-120::1]).flatten().max()<1000:
+        if len(y)<self.chunk:
+            y = np.pad(y,(0,self.chunk-len(y)),'constant')#数据维度需要和坐标维度一致
+            special_flag = True
+        self.data = np.append(self.data,np.array(y))
+
+        #默认最短3秒为每段话的间隔 3*1000/25*400=48000：只要说话内容间隔3秒以上即清除之前的
+        check_wav = self.data[-48000::1]
+        if not special_flag and check_wav.max()<30 and check_wav.min()>-30:
+            print('Start/Stop')
             if len(self.resHan)>0:#如果有语音识别结果则在最后清理之前输出（这个最后的输出是最完整的一句话）
                 print(self.resHan)#todo:或者给需要的地方
             self.data.clear()
             self.resHan.clear()
-        elif len(self.data)%40 == 0:#每1秒调用一次
-            wav = np.array(self.data).flatten()
-
+        elif len(self.data)%16000 == 0 or special_flag:#每1秒调用一次
             if True:#本地方式
-                pin,han = self.yysb.predict(wav)
+                pin,han = self.yysb.predict(self.data)
                 print('识别拼音：{}'.format(pin))
             else:#发送到服务器的方式
-                han = requests.post('http://127.0.0.1:20000/', {'token':'bringspring', 'wavs':wav,'pre_type':'W'})
+                han = requests.post('http://127.0.0.1:20000/', {'token':'bringspring', 'wavs':self.data,'pre_type':'W'})
                 han.encoding='utf-8'
 
-            print('识别汉字：{}'.format(han))
             self.resHan = han#每次都刷新识别结果，即最后一次的结果会是完整一句话
+            if special_flag:
+                print('识别汉字：{}'.format(han))#todo:或者给需要的地方
 
-        # 画波形图(上面的)
+        # 波形图(上面的)
         self.line1.set_data(x, y)
-        # 汉明窗结果（下面的）
-        freqs = np.linspace(0, self.rate // 2, self.chunk / 2)
+        # 时频图（下面的）
+        freqs = np.linspace(0, self.chunk, self.chunk / 2)
         _,_,xfp = utils.get_wav_Feature(wavsignal=y)
         self.line2.set_data(freqs, xfp)
         self._drawn_artists = [self.line1, self.line2]
@@ -103,5 +106,5 @@ class SubplotAnimation(animation.TimedAnimation):
 
 
 if __name__ == "__main__":
-    ani = SubplotAnimation('/media/yangjinming/DATA/Dataset/THCTS30/test/D4_751.wav')
+    ani = SubplotAnimation('/media/yangjinming/DATA/Dataset/THCTS30/dev/A11_101.wav')
     plt.show()
