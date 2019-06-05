fb293ef4f42f0295cfedebc4deccbbfb15572399
yangjinming1062 <944596544@qq.com>
2019-06-05 10:00:54 +0800

    修改测试

--- AboutDL/语音识别/test.py
+++ AboutDL/语音识别/test.py
@@ -3,27 +3,22 @@ import os
 import difflib
 import tensorflow as tf
 import numpy as np
-from utils import decode_ctc, GetEditDistance
+import utils
 
-
-# 0.准备解码所需字典，参数需和训练一致，也可以将字典保存到本地，直接进行读取
-from utils import get_data, data_hparams
-data_args = data_hparams()
-train_data = get_data(data_args)
-
+# 0.准备解码所需字典，参数需和训练一致
+data_args = utils.data_hparams()
+train_data = utils.get_data(data_args)
 
 # 1.声学模型-----------------------------------
 from model_speech.cnn_ctc import Am, am_hparams
-
 am_args = am_hparams()
 am_args.vocab_size = len(train_data.pny_vocab)
 am = Am(am_args)
 print('加载声学模型中...')
-am.ctc_model.load_weights('logs_am/model.h5')
+am.ctc_model.load_weights(os.path.join(utils.cur_path,'logs_am','model.h5'))
 
 # 2.语言模型-------------------------------------------
 from model_language.transformer import Lm, lm_hparams
-
 lm_args = lm_hparams()
 lm_args.input_vocab_size = len(train_data.pny_vocab)
 lm_args.label_vocab_size = len(train_data.han_vocab)
@@ -34,16 +29,14 @@ sess = tf.Session(graph=lm.graph)
 with lm.graph.as_default():
     saver =tf.train.Saver()
 with sess.as_default():
-    latest = tf.train.latest_checkpoint('logs_lm')
+    latest = tf.train.latest_checkpoint(os.path.join(utils.cur_path,'logs_lm'))
     saver.restore(sess, latest)
 
 # 3. 准备测试所需数据， 不必和训练数据一致，通过设置data_args.data_type测试，
-#    此处应设为'test'，我用了'train'因为演示模型较小，如果使用'test'看不出效果，
-#    且会出现未出现的词。
 data_args.data_type = 'train'
 data_args.shuffle = False
 data_args.batch_size = 1
-test_data = get_data(data_args)
+test_data = utils.get_data(data_args)
 
 # 4. 进行测试-------------------------------------------
 am_batch = test_data.get_am_batch()
@@ -57,7 +50,7 @@ for i in range(10):
     y = test_data.pny_lst[i]
     result = am.model.predict(x, steps=1)
     # 将数字结果转化为文本结果
-    _, text = decode_ctc(result, train_data.pny_vocab)
+    _, text = utils.decode_ctc(result, train_data.pny_vocab)
     text = ' '.join(text)
     print('文本结果：', text)
     print('原文结果：', ' '.join(y))
@@ -70,7 +63,7 @@ for i in range(10):
         got = ''.join(train_data.han_vocab[idx] for idx in preds[0])
         print('原文汉字：', label)
         print('识别结果：', got)
-        word_error_num += min(len(label), GetEditDistance(label, got))
+        word_error_num += min(len(label), utils.GetEditDistance(label, got))
         word_num += len(label)
 print('词错误率：', word_error_num / word_num)
 sess.close()
