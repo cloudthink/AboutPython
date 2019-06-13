14c1bdf81825962274c27a4c9cc5ba05b6b7dd7f
杨金明 <30614179+yangjinming1062@users.noreply.github.com>
2019-06-13 10:02:52 +0800

    服务器测试端对应修改

--- AboutDL/语音识别/testClient.py
+++ AboutDL/语音识别/testClient.py
@@ -6,7 +6,7 @@ data_args = utils.data_hparams()
 test = utils.get_data(data_args)
 
 _,wav = scipy.io.wavfile.read(os.path.join('/media/yangjinming/DATA/Dataset',test.wav_lst[0]))
-datas={'token':'bringspring', 'wavs':wav,'pre_type':'W'}
+datas={'token':'SR', 'data':wav,'pre_type':'H'}
 r = requests.post('http://127.0.0.1:20000/', datas)
 r.encoding='utf-8'
 
